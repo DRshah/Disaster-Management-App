cb7e79f9d8ce57b762577044824a4dd7892df5af
DRshah <shahdarsh239@gmail.com>
2018-03-26 13:42:50 +0530

    added new project

---
+++ DisasterMgmt/app/src/main/java/com/example/disastermanagement/datasource/ExpandableListDataSource.java
@@ -0,0 +1,31 @@
+package com.example.disastermanagement.datasource;
+
+import android.content.Context;
+
+import com.example.disastermanagement.R;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created by Darsh_Shah on 24-03-2018.
+ */
+
+public class ExpandableListDataSource {
+    public static Map<String, List<String>> getData(Context context) {
+        Map<String, List<String>> expandableListData = new TreeMap<>();
+        List<String> product_list = Arrays.asList(context.getResources().getStringArray(R.array.product_group));
+        List<String> vegetables = Arrays.asList(context.getResources().getStringArray(R.array.Vegetables));
+        List<String> vegetables2 = Arrays.asList(context.getResources().getStringArray(R.array.Vegetables));
+
+
+        expandableListData.put(product_list.get(0), vegetables);
+        //expandableListData.put(product_list.get(1),null);
+
+
+
+        return expandableListData;
+    }
+}
