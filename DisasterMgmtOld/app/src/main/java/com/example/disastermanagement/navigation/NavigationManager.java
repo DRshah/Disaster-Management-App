e597245c2f80fda3fa86c58ab7133b73e9e8ec03
Vaibhav Shah <vshah2212@gmail.com>
2018-03-31 15:11:52 +0530

    del

--- DisasterMgmtOld/app/src/main/java/com/example/disastermanagement/navigation/NavigationManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.disastermanagement.navigation;
-
-/**
- * Created by Darsh_Shah on 24-03-2018.
- */
-
-public interface NavigationManager {
-    void showFragmentAction(String title);
-}
